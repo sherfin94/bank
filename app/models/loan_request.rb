c8189957c77a86a9026d110adc7d3130522f5f78
Sherfin S <sherfin94@gmail.com>
2016-08-18 13:41:26 +0530

    Provide 'progress' method to the model LoanRequst
    The progress method returns the current progress
    of the loan request. It is calculated by assuming that
    10 seconds from the time of submission implies 100 percent.

--- app/models/loan_request.rb
+++ app/models/loan_request.rb
@@ -1,2 +1,5 @@
 class LoanRequest < ApplicationRecord
+  def progress
+    ((Time.now - self.request_time) * 10).round
+  end
 end
--- spec/models/loan_request_spec.rb
+++ spec/models/loan_request_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe LoanRequest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'progress' do
+    it 'returns 50 for a loan_request made 5 seconds ago' do
+      someGuy = LoanRequest.create!(request_time: Time.now - 5)
+      expect(someGuy.progress).to eq(50)
+    end
+  end
 end
