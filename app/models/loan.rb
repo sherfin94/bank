87cca941d1c0edc5d167d907a5146729dcfacdcc
Sherfin S <sherfin94@gmail.com>
2016-08-24 13:03:58 +0530

    Refactor model validation
    This commit arranges all the validate statements in loan model
    to a single statement.

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,10 +1,11 @@
 class Loan < ApplicationRecord
-  validates :borrower_name, presence: true
-  validates :principal_loan_amount, presence: true
-  validates :loan_number, presence: true
-  validates :first_payment_date, presence: true
-  validates :interest_rate, presence: true
-  validates :term, presence: true
+  validates :borrower_name,
+            :principal_loan_amount,
+            :loan_number,
+            :first_payment_date,
+            :interest_rate,
+            :term,
+            presence: true
   has_many :payments
   def progress
     ((Time.now - self.request_time) * 10).round
